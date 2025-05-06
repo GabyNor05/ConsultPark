e4c263bb58ad6d8c09a1962dd079474ea5ebb300
GabyNor05 <160027369+GabyNor05@users.noreply.github.com>
2025-05-06 10:35:00 +0200

    Added Navbar + File structure + Logo

--- consultpark/src/App.js
+++ consultpark/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './component/Navbar';
+import Home from './component/home/Home';
+import About from './component/about/About';
+import Services from './component/services/Services';
+import Outsourcing from './component/outsourcing/Outsourcing';
+import Contact from './component/contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/outsourcing" element={<Outsourcing />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
     </div>
+    
   );
 }
 
---
+++ consultpark/src/component/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/Consult-Park-Logo-Full.png";
+
+function Navbar() {
+  return (
+    <nav className="navbar" >
+      <img className = "logo" src={logo} alt="Consult Park"  />
+      <div className = "nav-link" >
+        <Link to="/" className="nav-link">Home</Link>
+        <Link to="/about" className="nav-link">About Us</Link>
+        <Link to="/services" className="nav-link">Services</Link>
+        <Link to="/outsouring" className="nav-link">Outsourcing</Link>
+        <Link to="/contact" className="nav-link">Contact Us</Link>
+      
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
