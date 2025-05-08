ed6d4f5e0213237521d9e832e3c8c0e1d812748c
KBOlifant <241299@virtualwindow.co.za>
2025-05-08 12:15:33 +0200

    Adding Outsourcing page and Contact us Page

--- consultpark/src/component/Services/Services.js
+++ consultpark/src/component/Services/Services.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './servicesComps/Services.css'; // Corrected path to Services.css
 import ServicesHeader from './servicesComps/ServicesHeader';
 import ServicesAccordion from './servicesComps/ServicesAccordion'; // Ensure this path is correct
-import Footer from '../home/Footer'; // Ensure this path is correct
+import Footer from '../Home/Footers'; // Ensure this path is correct
 
 function Services() {
     return(
