b11629a333d8a434d38912bf1850d6a195517726
KBOlifant <241299@virtualwindow.co.za>
2025-05-08 13:08:04 +0200

    Contact

--- consultpark/src/component/home/CTA_Button.js
+++ consultpark/src/component/home/CTA_Button.js
@@ -4,8 +4,8 @@ const Button = () => {
   return (
     <div >
         <div className='w-64 bg-green-700 m-auto rounded-full cursor-pointer btn transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-green-900 ...'>
-                <h2 className='p-6 text-zinc-50 text-center text-xl'>Contact Us</h2>
-    </div>
+          <h2 className='p-6 text-zinc-50 text-center text-xl'>Contact Us</h2>
+        </div>
     </div>
   );
 };
